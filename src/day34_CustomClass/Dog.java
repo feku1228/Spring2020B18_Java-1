e5f433ab43dc4b17c67ebcb3e6d46ee51824ca3e
cybertekschool <github@cybertekschool.com>
2020-05-05 11:52:20 -0400

    Day34 Class

---
+++ src/day34_CustomClass/Dog.java
@@ -0,0 +1,23 @@
+package day34_CustomClass;
+
+public class Dog {
+
+    String breed;
+    String size;
+    int age;
+    String color;
+    String name;
+
+
+    public void setDogInfo(String breed, String size, int age, String color, String name){
+       this.breed = breed;
+       this.size = size;
+       this.age = age;
+       this.color = color;
+       this.name = name;
+    }
+
+
+
+
+}
