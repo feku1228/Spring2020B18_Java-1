61ed839b70e63e54da28911aa8768d848be4ff08
cybertekschool <github@cybertekschool.com>
2020-05-05 11:06:15 -0400

    Day34 Class

--- src/day34_CustomClass/CarpetObjects.java
+++ src/day34_CustomClass/CarpetObjects.java
@@ -1,5 +1,8 @@
 package day34_CustomClass;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CarpetObjects {
 
     public static void main(String[] args) {
@@ -9,9 +12,32 @@ public class CarpetObjects {
                 carpets[1].customOrder(10, 15, 30, false);
                 carpets[2].customOrder(9,20,15,true);
                 carpets[3].customOrder(20,18,25,false);
-                carpets[4].customOrder(35, 45, 35, true);
+                carpets[4].customOrder(35, 45, 35, false);
 
-        System.out.println( carpets[4] );
+
+        ArrayList<Carpet> persianCarpets = new ArrayList<>();
+
+        for(Carpet eachCarpet  : carpets){
+            if(eachCarpet.isPersian){
+                persianCarpets.add(eachCarpet);
+            }
+        }
+
+        System.out.println("there are "+persianCarpets.size()+" persian carpets");
+
+        System.out.println(    persianCarpets.get(0).calcCost()    );
+        System.out.println(   persianCarpets.get(1).calcCost() );
+
+
+
+        ArrayList<Carpet> regularCarpets = new ArrayList<>(Arrays.asList(carpets) );
+        regularCarpets.removeAll(persianCarpets);
+
+        System.out.println("There are "+regularCarpets.size()+" regular carpets");
+
+        for(int i = 0; i < regularCarpets.size(); i++){
+            System.out.println( regularCarpets.get(i).calcCost()  );
+        }
 
 
 
