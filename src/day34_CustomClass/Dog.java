fb3387af675e8e3fb3af9d4e7ec8f5e3a3b98a4a
cybertekschool <github@cybertekschool.com>
2020-05-05 12:39:57 -0400

    Day34 Class

--- src/day34_CustomClass/Dog.java
+++ src/day34_CustomClass/Dog.java
@@ -8,6 +8,17 @@ public class Dog {
     String color;
     String name;
 
+    public void eating(String food){
+        System.out.println(name+" is eating "+food);
+    }
+
+    public void drinking(String drink){
+        System.out.println(name+" is drinking "+drink);
+    }
+
+    public void playing(){
+        System.out.println(name+" is playing");
+    }
 
     public void setDogInfo(String breed, String size, int age, String color, String name){
        this.breed = breed;
