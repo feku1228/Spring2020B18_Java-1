4fdb2ff68390b40cbfcc11af9f7f6080a84d3d9c
cybertekschool <github@cybertekschool.com>
2020-05-06 15:58:43 -0400

    Office: 05/06/2020

---
+++ src/OfficeHours/Practice_05_06_2020/AppleINC.java
@@ -0,0 +1,20 @@
+package OfficeHours.Practice_05_06_2020;
+
+public class AppleINC {
+
+    public static void main(String[] args) {
+        Tester tester1 = new Tester();
+        tester1.setInfo("Onur","Junior Tester",75000,1245);
+
+        Tester tester2 = new Tester();
+        tester2.setInfo("Elton John","Junior Tester",75000,1245);
+
+        Tester tester3 = new Tester();
+        tester1.setInfo("Rahman","SDET",120000,22222);
+
+
+
+
+    }
+
+}
