f1048aad0a9bdca61ffa1d9e5248c40333a515af
cybertekschool <github@cybertekschool.com>
2020-05-08 10:55:16 -0400

    Day36 class

--- src/day36_StaticBlock/Test.java
+++ src/day36_StaticBlock/Test.java
@@ -6,6 +6,9 @@ public class Test {
 
         System.out.println( StaticBlock2.tester1 );
 
+        System.out.println(staticBlock3.name );
+        System.out.println(staticBlock3.num);
+
 
     }
 }
