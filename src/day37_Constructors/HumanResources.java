7ff75c9980d57ad0a4d5bbe7956fc6f0545d94f6
cybertekschool <github@cybertekschool.com>
2020-05-11 10:30:09 -0400

    Day37 class

--- src/day37_Constructors/HumanResources.java
+++ src/day37_Constructors/HumanResources.java
@@ -16,7 +16,7 @@ public class HumanResources {
     static Employee employee4 = new Employee();
     static Employee employee5 = new Employee();
 
-
+/*
     public static void main(String[] args) {
         employee1.setInfo("Zuleyha",1254,45874641,"QA",110000,'F');
         employee2.setInfo("Kyle", 1123, 112345678, "SDET", 110000, 'M');
@@ -29,8 +29,25 @@ public class HumanResources {
         System.out.println(employee3);
         System.out.println(employee4);
         System.out.println(employee5);
+    }
+
+ */
+
+    static{
+
+        employee1.setInfo("Zuleyha",1254,45874641,"QA",110000,'F');
+        employee2.setInfo("Kyle", 1123, 112345678, "SDET", 110000, 'M');
+        employee3.setInfo("Marina", 1124, 111234567, "Product Manager", 170000, 'F');
+        employee4.setInfo("Adeline", 1125, 111123456, "Scrum Master", 80000, 'F');
+        employee5.setInfo("Jazi", 1126, 111112345, "Business Analyst", 70000, 'F');
+
 
     }
+
+
+
+
+
 
 
 
