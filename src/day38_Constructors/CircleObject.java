6baf63b6fc1e0bc255d509f91b169bdcbc98ce72
cybertekschool <github@cybertekschool.com>
2020-05-12 11:53:54 -0400

    Day38 class

--- src/day38_Constructors/CircleObject.java
+++ src/day38_Constructors/CircleObject.java
@@ -1,15 +1,20 @@
 package day38_Constructors;
 
+import java.util.Scanner;
+
 public class CircleObject {
 
     public static void main(String[] args) {
-       double r = 3;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle ");
+       double r = scan.nextDouble() ;
 
        Circle circle1 = new Circle(r);
 
        double areaOfCirlce = circle1.area();
-
-        System.out.println( areaOfCirlce  );
+        System.out.println("Diameter is: "+ circle1.diameter);
+        System.out.println("Perimeter is: "+ circle1.perimeter());
+        System.out.println("Area is: " + areaOfCirlce  );
 
     }
 }
