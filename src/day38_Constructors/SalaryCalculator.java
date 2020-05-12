07668099afafcf08aaf997ded580959729bf4820
cybertekschool <github@cybertekschool.com>
2020-05-12 10:51:25 -0400

    Day38 class

---
+++ src/day38_Constructors/SalaryCalculator.java
@@ -0,0 +1,23 @@
+package day38_Constructors;
+
+/*
+Create class called SalaryCalculator
+                instance variables:
+                    hourlyRate, weeklyHours, stateTaxRate, fedralTaxRate
+
+                add a constructor can initialize those fields
+
+                instance methods:
+                    salary(): returns the total salary as double  (hourlyRate * weeklyHours * 48)
+                    salaryAftertax(): retuns the salary after tax as double ( salary - stateTax - federalTax)
+                    stateTax(): retuns the total state tax as double ( salary * stateTaxRate)
+                    federalTax(): retuns the total federal tax as double ( salary * fedralTaxRate)
+
+                    toString(): returns the hourlyRate, weeklyHours and salary info as calculated by salary()
+
+ */
+
+public class SalaryCalculator {
+
+
+}
